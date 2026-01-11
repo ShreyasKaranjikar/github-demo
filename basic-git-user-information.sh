e4d6476424148f9713add6a416ea56e70c6d6c81
Shreyas Karanjikar <51901187+ShreyasKaranjikar@users.noreply.github.com>
2026-01-11 20:38:46 +0530

    Update basic-git-user-information.sh

--- basic-git-user-information.sh
+++ basic-git-user-information.sh
@@ -3,6 +3,10 @@
 username=`git config --global user.name`
 email_id=`git config --global user.email`
 
+history="This is history"
+
 echo "You have logged in with following details."
 echo "user name -" $username
 echo "with Email ID -" $email_id
+
+echo $history
