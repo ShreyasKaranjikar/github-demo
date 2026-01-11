3779f4d5786559be06f09188bdda33574733852f
ShreyasKaranjikar <karanjikar1997@gmail.com>
2026-01-11 20:28:02 +0530

    local : Added code to print commit logs at the end

--- basic-git-user-information.sh
+++ basic-git-user-information.sh
@@ -3,10 +3,11 @@
 username=`git config --global user.name`
 email_id=`git config --global user.email`
 
-history="This is history"
+history_log=`git hist`
 
 echo "You have logged in with following details."
 echo "user name -" $username
 echo "with Email ID -" $email_id
 
-echo $history
+printf "******************\nFollowing commits have been made so far\n******************\n${history_log}\n******************\n"
+
